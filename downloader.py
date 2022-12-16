e7431a63c4e9c60adb730f9223a679785a035e5d
TEGME1 <shaikfahad3210@gmail.com>
2022-12-16 18:34:19 +0530

    Extended the functionality to download videos with greater resolution
    Used moviepy module to merge the downloaded audio and video files. For now, only DASH videos can be downloaded(>=1080p).

--- downloader.py
+++ downloader.py
@@ -1,13 +1,25 @@
+import os
+
+from moviepy.editor import VideoFileClip, AudioFileClip
 from pytube import YouTube
 
-def Download(link):
-  ytObject=YouTube(link);
-  ytObject=ytObject.streams.get_highest_resolution()
-  try:
-    ytObject.download()
-  except:
-    print("Error occurred during download process")
-  print("Video has been downloaded check it out go!!!!!!!!!! 👍")
-    
-link=input("plz enter the YT video link here: ")
-Download(link)
+
+def download(url):
+    yt_object = YouTube(url)
+    video_title = yt_object.title
+
+    video = yt_object.streams.filter(mime_type="video/mp4").order_by('resolution').desc().first().download()
+    audio = yt_object.streams.filter(mime_type="audio/webm").order_by('itag').desc().first().download()
+
+    vid_clip = VideoFileClip(video)
+    aud_clip = AudioFileClip(audio)
+
+    merged_clip = vid_clip.set_audio(aud_clip)
+
+    merged_clip.write_videofile(f'{video_title} (downloaded).mp4')
+    os.remove(video)
+    os.remove(audio)
+
+
+link = input("Please enter the YouTube video link here: ")
+download(link)
